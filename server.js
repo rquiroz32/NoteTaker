e31336c9d1f6c1d9d4e92b3430c5956fc96b684b
rquiroz3290 <rquiroz3290@gmail.com>
2020-11-21 00:54:17 -0500

    fixed code where I was conflating built in node and express commands, got a basic request to a path working

--- server.js
+++ server.js
@@ -2,49 +2,52 @@
 const fs = require("fs")
 const express = require('express')
 const http = require('http');
-const { response } = require("express");
+const path = require("path");
+
+
 const app = express();
 
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 
 //Global Variables
-const server = http.createServer(handleRequest)
+//const server = http.createServer(handleRequest)
 const PORT = process.env.PORT || 8080
 
 
-// app.get('/test',function(req,res){
-//     console.log("test")
-//     console.log(req)
-// res.send("get received")
-//  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("/test", function (req, res) {
+    //let path = req.url
+    console.log("test")
+    console.log(req.url)
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 
 
-// });
+});
 
-function handleRequest(req,res){
+function handleRequest(req, res) {
+    // console.log("test")
+    // console.log(__dirname)
+    
 
-    //let path =req.url
-
-    app.get('/test',function(req,res){
-        console.log("test")
-        console.log(req)
-    res.send("get received")
-     res.sendFile(path.join(__dirname, "/public/index.html"))
+    // app.get("/test", function (req, res) {
+    //     console.log("test")
+    //     console.log(req)
+    //     res.send("get received")
+    //     res.sendFile(path.join(__dirname, "\\public\\index.html"))
     
     
-    });
-   
+    // });
+    
 
 }
 
 
-server.listen(PORT,function(){
+app.listen(PORT, function () {
 
-console.log(`Now listening on Port ${PORT}`)
-//handleRequest();
+    console.log(`Now listening on Port ${PORT}`)
+    //handleRequest();
 
 
 })
